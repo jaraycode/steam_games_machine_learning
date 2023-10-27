0d216573f6e13dc6d5cb60a5ce58253f71f0327e
jaraycode <jonasaray12@gmail.com>
2023-10-26 23:59:43 -0400

    Ya se puede descartar la información vacía

--- dataset.py
+++ dataset.py
@@ -16,22 +16,26 @@ class extraerJSON:
     
     def limpiar(self, entrada:[]) -> []:
         salida = []
-        
+        index = 0
+        isgood = True
+        # Mantener estructura de diccionario pero verificando que los datos estén limpios
         for element in entrada:
-            if isinstance(element, float):
-                if math.isnan(element):
-                    continue
-                else:
-                    salida.append(element)
+            for value in element.values():
+                if isinstance(value, float):
+                    if math.isnan(value):
+                        isgood = False
+            if isgood:
+                salida.append(entrada[index])
+                index+=1
             else:
-                salida.append(element)
+                isgood = True
+                index+=1
 
         return salida
 
-
 prueba = extraerJSON("Dataset/steam_games.json", "r")
-
+my = {}
 resultado = prueba.extraer("utf-8")
-
+resultado = prueba.limpiar(resultado)
 for a in resultado:
         print(a['developer'], type(a['developer']))
